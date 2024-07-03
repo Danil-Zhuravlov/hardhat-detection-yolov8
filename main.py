5d0d2a81f1d86c8de097755007178ffce2687478
Danil Zhuravlov <danil.zhuravlov.2004@gmail.com>
2024-07-03 13:59:00 +0200

    update comments, added config to teach the model

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("best.pt")  # load pretrained model from file
+model = YOLO("best.pt")
 
-
-# Use the model
-results = model(source=0, show=True, conf=0.75, save=True)
+# Use the model with a webcam
+results = model(source=0, show=True, conf=0.60, save=True)
--- train.py
+++ train.py
@@ -1,8 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8n.yaml")  # build a new model from scratch
+model = YOLO("yolov8n.yaml")  # choose a base model
 
-
-# Use the model
-results = model.train(data="config.yaml", epochs=50)  # train the model
+# Train the model
+results = model.train(data="config.yaml", epochs=50) # set epochs of 50~100
